99d08e237d85801df73a019e3a024aebc82f7849
hg_96 <hg_96@naver.com>
2024-09-25 00:20:13 +0900

    * 관리자 페이지 테스트 중

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import path from 'path';
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve('./src'), // '@' 별칭 설정
+      '@a': path.resolve('./src/app') // '@a' 별칭 설정
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ src/app/adm/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+
+export default function AdmPage() {
+  return (
+
+    <div>next 하자</div>
+  );
+}
